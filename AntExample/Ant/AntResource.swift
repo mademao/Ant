bee8a7b63d735ec720fd7f2e9f2e44e078432584
PlutoMa <mademaomail@126.com>
2017-07-11 16:10:54 +0800

    Add String+MD5.swift and AntResource property

--- AntExample/Ant/AntResource.swift
+++ AntExample/Ant/AntResource.swift
@@ -17,6 +17,18 @@ enum AntResourceState {
     case failed
 }
 
-class AntResource: NSObject {
-
+public class AntResource: NSObject {
+    let urlStr: String
+    var state: AntResourceState
+    let fileName: String
+    var writtenBytes: UInt64
+    var totalBytes: UInt64
+    
+    init(urlStr: String, state: AntResourceState, writtenBytes: UInt64, totalBytes: UInt64) {
+        self.urlStr = urlStr
+        self.state = state
+        self.fileName = urlStr.md5
+        self.writtenBytes = writtenBytes
+        self.totalBytes = totalBytes
+    }
 }
--- AntExample/AntExample/ViewController.swift
+++ AntExample/AntExample/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("123123123".md5)
     }
 }
 
