9437b15c502329835c6cb328fb1cad669cf7d9cc
PlutoMa <mademaomail@126.com>
2017-07-11 17:09:42 +0800

    Begin add property for AntDownloader

--- AntExample/Ant/AntDownloader.swift
+++ AntExample/Ant/AntDownloader.swift
@@ -8,6 +8,38 @@
 
 import UIKit
 
+/// 构建Session配置
+func createSessionConfiguration() -> URLSessionConfiguration {
+    let configuration = URLSessionConfiguration.default
+    configuration.httpShouldSetCookies = true
+    configuration.httpShouldUsePipelining = false
+    configuration.requestCachePolicy = .useProtocolCachePolicy
+    configuration.allowsCellularAccess = true
+    configuration.timeoutIntervalForRequest = 60.0
+    configuration.httpMaximumConnectionsPerHost = 10
+    configuration.isDiscretionary = true
+    return configuration
+}
+
 class AntDownloader: NSObject {
+    /// 串行线程
+    let synQueue = DispatchQueue(label: "cn.com.Pluto.AntDownloader", attributes: DispatchQueue.Attributes.concurrent)
+    /// 基础session
+    var session: URLSession?
+    /// 最大允许下载数
+    let allowMaxDownload: Int
+    /// 此时下载数
+    var currentDownload: Int = 0
+    
+    override init() {
+        self.allowMaxDownload = 4
+        super.init()
+        let optionQueue = OperationQueue()
+        optionQueue.maxConcurrentOperationCount = 1
+        self.session = URLSession(configuration: createSessionConfiguration(), delegate: self, delegateQueue: optionQueue)
+    }
+}
+
+extension AntDownloader: URLSessionDataDelegate {
     
 }
